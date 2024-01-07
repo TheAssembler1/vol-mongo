b68ff4f26c3f9e9aee98875c82c0084bbf2e0c6c
TheAssembler1 <theassembler@theassembler.theassembler.com>
2024-01-07 14:21:19 -0600

    vol_mongo commit on 2024-01-07 14:21:19

--- git_push.sh
+++ git_push.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+# Get the current date
+current_date=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Perform the git operations
 git add .
-git commit -m "vol_mongo commit"
+git commit -m "vol_mongo commit on $current_date"
 git push
